2c28f1f8b401f30d8bb87748fd8f6c1841b594de
Brendan Allan <brendonovich@outlook.com>
2023-04-29 01:46:08 +0800

    function + result

--- src/functions/result.rs
+++ src/functions/result.rs
@@ -2,31 +2,47 @@ use std::future::Future;
 
 use crate::{DataType, DefOpts, ExportError, Type};
 
+#[derive(Debug, Clone)]
+pub enum SpectaFunctionResultVariant {
+    Value(DataType),
+    Result(DataType, DataType),
+}
+
 /// Implemented by types that can be returned from a function annotated with
 /// [`specta`](crate::specta).
 pub trait SpectaFunctionResult<TMarker> {
     /// Gets the type of the result as a [`DataType`].
-    fn to_datatype(opts: DefOpts) -> Result<DataType, ExportError>;
+    fn to_datatype(opts: DefOpts) -> Result<SpectaFunctionResultVariant, ExportError>;
 }
 
-#[cfg(feature = "serde")]
 #[doc(hidden)]
-pub enum SpectaFunctionResultSerialize {}
-
-#[cfg(feature = "serde")]
-impl<T: serde::Serialize + Type> SpectaFunctionResult<SpectaFunctionResultSerialize> for T {
-    fn to_datatype(opts: DefOpts) -> Result<DataType, ExportError> {
-        T::reference(opts, &[])
+pub enum SpectaFunctionResultType {}
+impl<T: Type> SpectaFunctionResult<SpectaFunctionResultType> for T {
+    fn to_datatype(opts: DefOpts) -> Result<SpectaFunctionResultVariant, ExportError> {
+        T::reference(opts, &[]).map(SpectaFunctionResultVariant::Value)
     }
 }
 
 #[doc(hidden)]
-pub struct SpectaFunctionResultResult<TMarker>(TMarker);
-impl<TMarker, T: SpectaFunctionResult<TMarker>, E>
-    SpectaFunctionResult<SpectaFunctionResultResult<TMarker>> for Result<T, E>
-{
-    fn to_datatype(opts: DefOpts) -> Result<DataType, ExportError> {
-        T::to_datatype(opts)
+pub struct SpectaFunctionResultResult {}
+impl<T: Type, E: Type> SpectaFunctionResult<SpectaFunctionResultResult> for Result<T, E> {
+    fn to_datatype(opts: DefOpts) -> Result<SpectaFunctionResultVariant, ExportError> {
+        Ok(SpectaFunctionResultVariant::Result(
+            T::reference(
+                DefOpts {
+                    parent_inline: opts.parent_inline,
+                    type_map: opts.type_map,
+                },
+                &[],
+            )?,
+            E::reference(
+                DefOpts {
+                    parent_inline: opts.parent_inline,
+                    type_map: opts.type_map,
+                },
+                &[],
+            )?,
+        ))
     }
 }
 
@@ -35,7 +51,7 @@ pub struct SpectaFunctionResultFuture<TMarker>(TMarker);
 impl<TMarker, T: SpectaFunctionResult<TMarker>, TFut: Future<Output = T>>
     SpectaFunctionResult<SpectaFunctionResultFuture<TMarker>> for TFut
 {
-    fn to_datatype(opts: DefOpts) -> Result<DataType, ExportError> {
+    fn to_datatype(opts: DefOpts) -> Result<SpectaFunctionResultVariant, ExportError> {
         T::to_datatype(opts)
     }
 }
