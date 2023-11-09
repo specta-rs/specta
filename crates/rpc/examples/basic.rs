ea0f15d00227ce73416ad6e0e67c6ce13d1905a6
Oscar Beaumont <oscar@otbeaumont.me>
2023-11-09 20:09:01 +0800

    sync + async handlers for basic example

--- crates/rpc/examples/basic.rs
+++ crates/rpc/examples/basic.rs
@@ -1,11 +1,25 @@
-use std::marker::PhantomData;
+use std::{fmt::Debug, future::Future, marker::PhantomData};
 
+use specta::Type;
 use specta_rpc::router;
 
 pub trait Handler<Ctx, M> {}
 
-pub struct Marker<T, R>(PhantomData<(T, R)>);
-impl<Ctx, T, R, F: Fn(Ctx, T) -> R> Handler<Ctx, Marker<T, R>> for F {}
+pub struct SyncMarker<T, R>(PhantomData<(T, R)>);
+impl<TContext, TArgument, R: Debug + Type, F: Fn(TContext, TArgument) -> R>
+    Handler<TContext, SyncMarker<TArgument, R>> for F
+{
+}
+
+pub struct AsyncMarker<TFunction>(PhantomData<TFunction>);
+impl<TContext, TArgument, TFunction, TFuture> Handler<TContext, AsyncMarker<TArgument>>
+    for TFunction
+where
+    TFunction: Fn(TContext, TArgument) -> TFuture,
+    TFuture: Future,
+    TFuture::Output: Debug,
+{
+}
 
 router!(Router::<Ctx> [H, M] where H: Handler<Ctx, M>);
 
@@ -13,4 +27,8 @@ fn main() {
     let router = Router::<String>::new()
         .mount("demo", |ctx, arg: ()| "Hello, World Sync!")
         .mount("demo", |ctx, arg: ()| async move { "Hello, World Async!" });
+
+    // TODO: Export types
+
+    // TODO: Runtime demo
 }
