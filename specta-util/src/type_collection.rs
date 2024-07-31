168dc3502565e98477c65dbfdc08b55471f7314b
Oscar Beaumont <oscar@otbeaumont.me>
2024-07-31 21:18:02 +0800

    wip

--- specta-util/src/type_collection.rs
+++ specta-util/src/type_collection.rs
@@ -1,17 +1,24 @@
-use std::{borrow::Borrow, collections::HashMap, path::Path};
+use std::{
+    borrow::{Borrow, Cow},
+    collections::HashMap,
+    path::Path,
+};
 
 use specta::{Language, NamedDataType, NamedType, SpectaID, TypeMap};
 
 /// Define a set of types which can be exported together
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct TypeCollection {
+    // TODO: Make into a single `Vec<Enum>` to maintain global ordering?
     types: HashMap<SpectaID, fn(&mut TypeMap) -> NamedDataType>,
+    constants: HashMap<Cow<'static, str>, serde_json::Value>, // TODO: Can we make this format agnostic?
 }
 
 impl Default for TypeCollection {
     fn default() -> Self {
         Self {
-            types: HashMap::new(),
+            types: Default::default(),
+            constants: Default::default(),
         }
     }
 }
@@ -19,19 +26,47 @@ impl Default for TypeCollection {
 impl TypeCollection {
     #[allow(unused)]
     pub(crate) fn from_raw(types: HashMap<SpectaID, fn(&mut TypeMap) -> NamedDataType>) -> Self {
-        Self { types }
+        Self {
+            types,
+            constants: Default::default(),
+        }
     }
+
+    // TODO: Maybe register framework info for default header
 
     /// Join another type collection into this one.
     pub fn extend(&mut self, collection: impl Borrow<TypeCollection>) -> &mut Self {
-        self.types.extend(collection.borrow().types.iter());
+        let collection = collection.borrow();
+        self.types.extend(collection.types.iter());
+        self.constants.extend(
+            collection
+                .constants
+                .iter()
+                .map(|(k, v)| (k.clone(), v.clone())),
+        );
         self
     }
+
+    // TODO: Should you be allowed to merge in a `TypeMap`???
+
+    // TODO: Should you be able to output a type_map from our internal registry
 
     /// Register a type with the collection.
     pub fn register<T: NamedType>(&mut self) -> &mut Self {
         self.types
             .insert(T::sid(), |type_map| T::definition_named_data_type(type_map));
+        self
+    }
+
+    /// TODO
+    // #[cfg(feature = "serde")] // TODO
+    pub fn constant<T: serde::Serialize>(
+        &mut self,
+        name: impl Into<Cow<'static, str>>,
+        value: T,
+    ) -> &mut Self {
+        self.constants
+            .insert(name.into(), serde_json::to_value(value).unwrap()); // TODO: Error handling
         self
     }
 
@@ -47,9 +82,10 @@ impl TypeCollection {
     pub fn export<L: Language>(&self, language: L) -> Result<String, L::Error> {
         let mut type_map = TypeMap::default();
         self.collect(&mut type_map);
-        language.export(type_map)
+        language.export(type_map) // TODO: &self.constants
     }
 
+    // TODO: Maybe we could put `path` on `Language` and remove this?
     /// TODO
     pub fn export_to<L: Language>(
         &self,
