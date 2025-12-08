e46aa0d5153c24e47cdbf573d56b315e3598b5cc
Oscar Beaumont <oscar@otbeaumont.me>
2025-12-08 10:49:06 +0800

    AI slop

--- specta-zod/src/error.rs
+++ specta-zod/src/error.rs
@@ -1,10 +1,9 @@
 use core::fmt;
-use std::borrow::Cow;
+use std::{borrow::Cow, panic::Location};
 
-use specta_serde::SerdeError;
 use thiserror::Error;
 
-use crate::{context::ExportPath, ImplLocation};
+use crate::context::ExportPath;
 
 /// Describes where an error occurred.
 #[derive(Error, Debug, PartialEq)]
@@ -24,16 +23,14 @@ impl fmt::Display for NamedLocation {
     }
 }
 
-/// The error type for the TypeScript exporter.
+/// The error type for the Zod exporter.
 #[derive(Error, Debug)]
 #[non_exhaustive]
-pub enum ExportError {
+pub enum Error {
     #[error("Attempted to export '{0}' but Specta configuration forbids exporting BigInt types (i64, u64, i128, u128) because we don't know if your se/deserializer supports it. You can change this behavior by editing your `ExportConfiguration`!")]
     BigIntForbidden(ExportPath),
     #[error("Serde error: {0}")]
-    Serde(#[from] SerdeError),
-    // #[error("Attempted to export '{0}' but was unable to export a tagged type which is unnamed")]
-    // UnableToTagUnnamedType(ExportPath),
+    Serde(#[from] specta_serde::Error),
     #[error("Attempted to export '{1}' but was unable to due to {0} name '{2}' conflicting with a reserved keyword in Typescript. Try renaming it or using `#[specta(rename = \"new name\")]`")]
     ForbiddenName(NamedLocation, ExportPath, &'static str),
     #[error("Attempted to export '{1}' but was unable to due to {0} name '{2}' containing an invalid character")]
@@ -42,8 +39,11 @@ pub enum ExportError {
     InvalidTagging(ExportPath),
     #[error("Attempted to export '{0}' with internal tagging but the variant is a tuple struct.")]
     InvalidTaggedVariantContainingTupleStruct(ExportPath),
-    #[error("Unable to export type named '{0}' from locations '{:?}' '{:?}'", .1.as_str(), .2.as_str())]
-    DuplicateTypeName(Cow<'static, str>, ImplLocation, ImplLocation),
+    #[error("Unable to export type named '{name}' from multiple locations")]
+    DuplicateTypeName {
+        name: Cow<'static, str>,
+        types: (Location<'static>, Location<'static>),
+    },
     #[error("IO error: {0}")]
     Io(#[from] std::io::Error),
     #[error("Failed to export '{0}' due to error: {1}")]
@@ -51,12 +51,11 @@ pub enum ExportError {
 }
 
 // TODO: This `impl` is cringe
-impl PartialEq for ExportError {
+impl PartialEq for Error {
     fn eq(&self, other: &Self) -> bool {
         match (self, other) {
             (Self::BigIntForbidden(l0), Self::BigIntForbidden(r0)) => l0 == r0,
             (Self::Serde(l0), Self::Serde(r0)) => l0 == r0,
-            // (Self::UnableToTagUnnamedType(l0), Self::UnableToTagUnnamedType(r0)) => l0 == r0,
             (Self::ForbiddenName(l0, l1, l2), Self::ForbiddenName(r0, r1, r2)) => {
                 l0 == r0 && l1 == r1 && l2 == r2
             }
@@ -68,8 +67,8 @@ impl PartialEq for ExportError {
                 Self::InvalidTaggedVariantContainingTupleStruct(l0),
                 Self::InvalidTaggedVariantContainingTupleStruct(r0),
             ) => l0 == r0,
-            (Self::DuplicateTypeName(l0, l1, l2), Self::DuplicateTypeName(r0, r1, r2)) => {
-                l0 == r0 && l1 == r1 && l2 == r2
+            (Self::DuplicateTypeName { name: l0, types: l1 }, Self::DuplicateTypeName { name: r0, types: r1 }) => {
+                l0 == r0 && l1 == r1
             }
             (Self::Io(l0), Self::Io(r0)) => l0.to_string() == r0.to_string(), // This is a bit hacky but it will be fine for usage in unit tests!
             (Self::Other(l0, l1), Self::Other(r0, r1)) => l0 == r0 && l1 == r1,
