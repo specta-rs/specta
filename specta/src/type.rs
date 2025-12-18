0830c1cbceb0acb33863963b9d4c495c67fb3c7b
Oscar Beaumont <oscar@otbeaumont.me>
2025-12-18 13:43:26 +0800

    `diagnostic::on_unimplemented` - closes #401

--- specta/src/type.rs
+++ specta/src/type.rs
@@ -11,6 +11,15 @@ mod legacy_impls;
 ///
 /// This should be only implemented by the [`Type`](derive@crate::Type) macro.
 /// TODO: Discuss how to avoid custom implementations.
+#[diagnostic::on_unimplemented(
+    message = "the trait `specta::Type` is not implemented for `{Self}`",
+    label = "`{Self}` must implement `Type`",
+    note = "Depending on your use case, this can be fixed in multiple ways:
+ - If your using an type defined in one of your own crates, ensure you have `#[derive(specta::Type)]` on it.
+ - If your using a crate with official Specta support enable the feature flag on the 'specta' crate, refer to the documentation at https://docs.rs/specta/latest/specta/#feature-flags.
+ - If your using an external crate without Specta support, you may need to wrap your type in a new-type wrapper, refer to the examples at https://docs.rs/specta/latest/specta/trait.Type.html
+"
+)]
 pub trait Type {
     /// returns a [`DataType`](crate::datatype::DataType) that represents the type.
     /// This will also register this and any dependent types into the [`TypeCollection`].
