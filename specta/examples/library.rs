7d50becc7e1ab712f0735d1d6ebd880a727d0e95
Oscar Beaumont <oscar@otbeaumont.me>
2026-01-06 13:15:40 +0800

    remove `specta/examples`

--- specta/examples/library.rs
+++
@@ -1,7 +0,0 @@
-//! This example shows how to use the low-level Specta API to build a library on top of Specta.
-//!
-//! If your an end-user you should refer to the examples in `/specta-typescript/examples` instead.
-
-fn main() {
-    todo!();
-}
