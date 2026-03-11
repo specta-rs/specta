8034e3400c1b09209b283130b7207c9c995099e0
Oscar Beaumont <oscar@otbeaumont.me>
2026-03-11 16:22:43 +0800

    add some more serde tests

---
+++ tests/tests/serde_identifiers.rs
@@ -0,0 +1,66 @@
+use serde::Deserialize;
+use specta::{Type, TypeCollection};
+use specta_typescript::{BigIntExportBehavior, Typescript};
+
+#[derive(Type, Deserialize)]
+#[specta(collect = false)]
+#[serde(variant_identifier, rename_all = "snake_case")]
+enum VariantIdentifier {
+    HttpStatus,
+    #[serde(alias = "legacy")]
+    LegacyName,
+}
+
+#[derive(Type, Deserialize)]
+#[specta(collect = false)]
+#[serde(field_identifier, rename_all = "snake_case")]
+enum FieldIdentifier {
+    FirstName,
+    LastName,
+    Other(bool),
+}
+
+#[test]
+fn identifier_apply_requires_phases() {
+    let err = specta_serde::apply(TypeCollection::default().register::<VariantIdentifier>())
+        .expect_err("variant_identifier should require apply_phases");
+    assert!(err
+        .to_string()
+        .contains("identifier enums require `apply_phases`"));
+
+    let err = specta_serde::apply(TypeCollection::default().register::<FieldIdentifier>())
+        .expect_err("field_identifier should require apply_phases");
+    assert!(err
+        .to_string()
+        .contains("identifier enums require `apply_phases`"));
+}
+
+#[test]
+fn identifier_apply_phases_exports_deserialize_union() {
+    let variant_types =
+        specta_serde::apply_phases(TypeCollection::default().register::<VariantIdentifier>())
+            .expect("variant_identifier should be supported by apply_phases");
+    let variant_ts = Typescript::default()
+        .bigint(BigIntExportBehavior::Number)
+        .export(&variant_types)
+        .expect("typescript export should succeed");
+
+    assert!(variant_ts.contains("VariantIdentifier_Deserialize"));
+    assert!(variant_ts.contains("\"http_status\""));
+    assert!(variant_ts.contains("\"legacy\""));
+    assert!(variant_ts.contains("number"));
+
+    let field_types =
+        specta_serde::apply_phases(TypeCollection::default().register::<FieldIdentifier>())
+            .expect("field_identifier should be supported by apply_phases");
+    let field_ts = Typescript::default()
+        .bigint(BigIntExportBehavior::Number)
+        .export(&field_types)
+        .expect("typescript export should succeed");
+
+    assert!(field_ts.contains("FieldIdentifier_Deserialize"));
+    assert!(field_ts.contains("\"first_name\""));
+    assert!(field_ts.contains("\"last_name\""));
+    assert!(field_ts.contains("number"));
+    assert!(field_ts.contains("boolean"));
+}
