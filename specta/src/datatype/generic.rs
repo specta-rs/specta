ada879444f24cae02c7d18c9979d763b72277dd6
Oscar Beaumont <oscar@otbeaumont.me>
2026-04-09 13:28:56 +0800

    base

--- specta/src/datatype/generic.rs
+++ specta/src/datatype/generic.rs
@@ -3,18 +3,10 @@ use std::{any::TypeId, borrow::Cow};
 use crate::datatype::{DataType, GenericReference};
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
-pub enum GenericType {
-    Type,
-    Const { ty: DataType },
-}
-
-// TODO: Sealing fields
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct Generic {
     id: TypeId,
     pub name: Cow<'static, str>,
     pub default: Option<DataType>,
-    pub inner: GenericType,
 }
 
 impl Generic {
@@ -23,20 +15,6 @@ impl Generic {
             id: TypeId::of::<T>(),
             name,
             default,
-            inner: GenericType::Type,
-        }
-    }
-
-    pub fn new_const<T: 'static>(
-        name: Cow<'static, str>,
-        ty: DataType,
-        default: Option<DataType>,
-    ) -> Self {
-        Self {
-            id: TypeId::of::<T>(),
-            name,
-            default,
-            inner: GenericType::Const { ty },
         }
     }
 
