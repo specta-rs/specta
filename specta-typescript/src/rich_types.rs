8a92e669393a1b4c36deae9dd5bc3e18451c3264
Oscar Beaumont <oscar@otbeaumont.me>
2026-05-06 17:13:23 +0800

    flatten rich type code

--- specta-typescript/src/rich_types.rs
+++ specta-typescript/src/rich_types.rs
@@ -5,8 +5,6 @@ use specta::{
     datatype::{DataType, Fields, NamedReferenceType, Primitive, Reference},
 };
 
-mod rules;
-
 /// A rich type runtime JS transformer function.
 ///
 /// This defines a JS function which can convert between the incoming/outgoing type and it's JSON representation.
@@ -98,6 +96,42 @@ pub struct RichTypesConfiguration {
     rules: Vec<Rule>,
     lossless_bigint: bool,
     lossless_floats: bool,
+}
+
+impl Default for RichTypesConfiguration {
+    fn default() -> Self {
+        Self {
+            rules: vec![
+                // Uint8Array
+                Rule {
+                    name: "Bytes".into(),
+                    module_path: "bytes".into(),
+                    data_type: DataTypeFn::new(|_| define("Uint8Array").into()),
+                    serialize: Some(Transform::new(|i| format!("[...{i}]"))),
+                    deserialize: Some(Transform::new(|i| format!("new Uint8Array({i})"))),
+                },
+                Rule {
+                    name: "BytesMut".into(),
+                    module_path: "bytes".into(),
+                    data_type: DataTypeFn::new(|_| define("Uint8Array").into()),
+                    serialize: Some(Transform::new(|i| format!("[...{i}]"))),
+                    deserialize: Some(Transform::new(|i| format!("new Uint8Array({i})"))),
+                },
+                // Date
+                Rule {
+                    name: "DateTime".into(),
+                    module_path: "chrono".into(),
+                    data_type: DataTypeFn::new(|_| define("Date").into()),
+                    serialize: None,
+                    deserialize: Some(Transform::new(|i| format!("new Date({i})"))),
+                },
+                // TODO: `NaiveDate`, `NaiveDateTime`, `DateTime<FixedOffset>`
+                // https://chatgpt.com/c/69fae901-2590-839c-8174-cfca70cc23bc
+            ],
+            lossless_bigint: false,
+            lossless_floats: false,
+        }
+    }
 }
 
 impl RichTypesConfiguration {
